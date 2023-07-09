2447fd48fb34170ad0d7247957d1c8699e2b003d
Tomas Waller <wtomik@gmail.com>
2023-07-09 13:41:08 +0200

    adding first wait

--- src/main/java/sazka/ios/test/IOSTest.java
+++ src/main/java/sazka/ios/test/IOSTest.java
@@ -5,6 +5,7 @@ import sazka.ios.test.driver.IOS;
 
 public class IOSTest {
     protected IOS driver;
+    protected final int DEFAULT_TIMEOUT = 60;
 
     @BeforeSuite
     public void installApp() {
--- src/test/java/sazka/ios/test/LoginTest.java
+++ src/test/java/sazka/ios/test/LoginTest.java
@@ -1,11 +1,19 @@
 package sazka.ios.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTest extends IOSTest {
 
     @Test
     public void login() {
         driver.activateApp();
+        new WebDriverWait(driver.getDriver(), Duration.ofSeconds(DEFAULT_TIMEOUT))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("cz.sazka.loterie.uat:id/btnItem")))
+                .click();
     }
 }
