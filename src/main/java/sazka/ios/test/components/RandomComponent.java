f94163a1e27a9b75f5b4332ffece29cea4fa4fb2
Tomas Waller <wtomik@gmail.com>
2023-10-13 21:35:08 +0200

    refactoring

--- src/main/java/sazka/ios/test/components/RandomComponent.java
+++ src/main/java/sazka/ios/test/components/RandomComponent.java
@@ -1,7 +1,6 @@
 package sazka.ios.test.components;
 
 import sazka.ios.test.base.PageComponent;
-import sazka.ios.test.base.annotations.Locator;
 import sazka.ios.test.base.driver.Driver;
 
 public class RandomComponent extends PageComponent {
